03dd7b01db72cdc025c63b136b21b295c1bfc7c4
haki-projects <30647767+haki-projects@users.noreply.github.com>
2017-08-04 17:56:25 +0200

    book reducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    {title: 'Javascript: The Good Parts'},
+    {title: 'Harry Potter'},
+    {title: 'Elders Game'},
+    {title: 'The Dark Tower'},
+    {title: 'Eloquent Ruby'},
+  ]
+}
